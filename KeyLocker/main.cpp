2bc49dbccb3e9644f02f869f1e57a025f28f9a46
AlexOS12 <sluhach2016@gmail.com>
2024-02-09 21:49:02 +0300

    Пока не оч работает

--- KeyLocker/main.cpp
+++ KeyLocker/main.cpp
@@ -1,11 +1,37 @@
 #include "authwindow.h"
+#include "mainwindow.h"
 
+#include <QObject>
 #include <QApplication>
+
+class Initiator {
+    Q_OBJECT
+private:
+    AuthWindow authWindow;
+
+public slots:
+    void initialize(bool authResult) {
+        if (authResult) {
+            MainWindow mainWindow;
+            mainWindow.show();
+        } else {
+            qDebug() << "wrong pin";
+        }
+    }
+
+public:
+    Initiator() {
+        QObject::connect(&authWindow, SIGNAL(sendAuthResult(bool)), this, SLOT(initialize(bool)));
+        authWindow.show();
+    }
+
+};
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    AuthWindow authWindow;
-    authWindow.show();
+
+    Initiator initiator;
+
     return a.exec();
 }
